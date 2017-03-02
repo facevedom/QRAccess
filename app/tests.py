130bfded4a8272cab9eae997fb5e14fa10bfe65b
Sebastian <svillegasz@unal.edu.co>
2017-03-02 15:36:06 -0500

    Trying to push wsgi.py

--- app/tests.py
+++ app/tests.py
@@ -39,11 +39,13 @@ class ViewTest(TestCase):
         # tests if the POST request was succesfully received
         random_email = random_string(8) + '@domain.com'
         random_id = random_int()
-        response = self.client.post('/user/registration', {'email': random_email,
-                                    'id': random_id,
-                                    'name': 'Peter',
-                                    'last_name': 'Retep',
-                                    'event_id': 'RutaN_0045HACK'
+        response = self.client.post('/user/registration', 
+                                    {
+                                        'email': random_email,
+                                        'id': random_id,
+                                        'name': 'Peter',
+                                        'last_name': 'Retep',
+                                        'event_id': 'RutaN_0045HACK'
                                     })
         self.assertContains(response, 'Congratulations', 1, 200)
 
