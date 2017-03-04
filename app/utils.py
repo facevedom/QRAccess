35b1937e3965a32961692a9086f9e79778fb53c5
Felipe Acevedo <facevedom@unal.edu.co>
2017-03-03 22:34:46 -0500

    More testing and home page

--- app/utils.py
+++ app/utils.py
@@ -6,7 +6,6 @@ import string
 def random_string(length):
     return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
-
 # generates a random int, with optional range
 def random_int(min=0, max=9999999999):
-    return random.randrange(min, max)
+    return random.randint(min, max)
