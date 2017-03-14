8947ba95a16d7c9d9ecdbf89ae50dc989ffd3cfb
Felipe Acevedo <facevedom@unal.edu.co>
2017-03-14 08:38:52 -0500

    Login template, added new tests

--- QRAccess/urls.py
+++ QRAccess/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from datetime import datetime
 
 import app.forms
 import app.views
@@ -20,7 +21,7 @@ urlpatterns = [
     url(r'^user/registration$', app.views.user_registration, name='registration'),
     url(r'^generate/([A-Za-z0-9]+)$', app.views.generate_qr, name='generate'),
     url(r'^check-access$', app.views.check_room_access),
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^login/$', auth_views.login, {'extra_context':{'year':datetime.now().year}}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
